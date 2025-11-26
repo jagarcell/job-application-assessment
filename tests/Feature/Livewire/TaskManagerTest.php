963d137e0c60760cbb0375b9e6d5596d8d8306d6
Jorge Garcell <jagarcell@gmail.com>
2025-11-26 16:09:45 -0500

    test livewire component exists and is renderable

---
+++ tests/Feature/Livewire/TaskManagerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use Livewire\Livewire;
+use App\Livewire\TasksManager;
+
+it('tests if Livewire task manager component exists in the home page', function () {
+    /** @var \Tests\TestCase $this */
+    $this->get('/')->assertSeeLivewire(TasksManager::class);
+});
+
+it('tests if Livewire TasksManager component can render', function () {
+    Livewire::test(TasksManager::class)
+        ->assertStatus(200);
+});
